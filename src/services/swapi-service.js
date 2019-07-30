2a855e1a5d25558245c9a214d8e53b4d12170863
pyrigok <pyrigok@i.ua>
2019-07-30 16:08:20 +0300

    folder structure added

---
+++ src/services/swapi-service.js
@@ -0,0 +1,51 @@
+
+export default class SwapiService {
+
+    _apiBase = 'https://swapi.co/api';
+
+    async getResource(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}.`)
+        }
+    }
+
+    async getAllPeople() {
+        const res = await this.getResource(`/people/`);
+        return res.results;
+    }
+
+    getPerson(id) {
+        return this.getResource(`/people/${id}/`);
+    }
+
+    async getPlanets() {
+        const res = await this.getResource(`/planets/`);
+        return res.results;
+    }
+
+    getPlanet(id) {
+        return this.getResource(`/planets/${id}/`);
+    }
+
+      async getStarships() {
+        const res = await this.getResource(`/starships/`);
+        return res.results;
+    }
+
+    getStarship(id) {
+        return this.getResource(`/starships/${id}/`);
+    }
+
+}
+
+const swapi = new SwapiService();
+
+swapi.getAllPeople().then((people) => {
+    people.forEach((p) => {
+        console.log(p.name);
+    })
+    ;
+});
+
